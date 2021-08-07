141a5e7ec2634f2c3f90ca671b45181cc031784e
Nilton Santos <niltonsantosmf2@gmail.com>
2021-08-07 16:46:04 -0300

    alterações

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -4,24 +4,46 @@ import { RectButton, RectButtonProps } from 'react-native-gesture-handler';
 
 import { styles } from './styles'
 
-type Props = RectButtonProps & {
-    title: string;
+// type Props = RectButtonProps & {
+//     title: string;
+// }
+
+interface Props {
+    
+};
+
+
+export function Card(){
+    return(
+        <View>
+            <View style={styles.container}> 
+                <Text>Pedreiros</Text>
+            </View>
+
+            <View style={styles.CardImage}>
+            
+            </View>
+
+        </View>
+
+       
+    )
 }
 
-export function Card({title, ...rest}: Props){
-    return(
-        <View style={styles.container}>
-            <RectButton
-                style={styles.cardButton}
-                {...rest}
-            >
-                <Text
-                    style={styles.textCard}
-                >
-                    {title}
-                </Text>    
-            </RectButton>
-        </View>
-    )
+// export function Card({title, ...rest}: Props){
+//     return(
+//         <View style={styles.container}>
+//             <RectButton
+//                 style={styles.cardButton}
+//                 {...rest}
+//             >
+//                 <Text
+//                     style={styles.textCard}
+//                 >
+//                     {title}
+//                 </Text>    
+//             </RectButton>
+//         </View>
+//     )
     
-}
+// }
